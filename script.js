c775ed564406e66e818f4b58d2e0357e0389cd64
dheeraz15 <dhirajchapagain353@gmail.com>
2025-09-03 11:18:12 +0545

    even

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Script loaded successfully.");
+
+
+function sum(a,b){
+    return  a + b;
+}
+function difference(c,d){
+    return c - d;
+}
+
+function divison(e,f){
+    return e / f;
+}
+
+function product(g,h){
+return g * h;
+}
+
+console.log("Sum is: ", sum(5,3));
+console.log("Difference is: ", difference(5,3));
+console.log("Division is: ", divison(6,3));
+console.log("Product is: ", product(5,3));
