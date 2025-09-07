340dfe88b8a05cc6a3adf4963e9f68b93c4d637f
dheeraz15 <dhirajchapagain353@gmail.com>
2025-09-07 11:22:50 +0545

    class: 7th sept

--- even.js
+++ even.js
@@ -1,6 +1,43 @@
-let number = prompt("Enter a number:");
-if (number % 2 === 0) {
-    console.log(`${number} is even.`);
-} else {
-    console.log(`${number} is odd.`);
+// let number = prompt("Enter a number:");
+// if (number % 2 === 0) {
+//     console.log(`${number} is even.`);
+// } else {
+//     console.log(`${number} is odd.`);
+// }
+
+
+// day = "Sunday";
+
+
+// let x = '5';
+// if (x === 5) {
+//     console.log("Inside if");
+// }
+
+// if (month == "September" || day == "Sunday" ){
+// console.log ("Class Day");
+// }
+// else {
+// console.log ("The class is off!")
+// }
+
+let day1 = prompt("Enter a day: ");
+let day = day1.toLowerCase();
+if(day == "saturday") {
+    console.log("Holiday");
+}
+else if (day == "sunday" || day == "wednesday") {
+    console.log("Web Dev Class Day");
+}
+else {
+    console.log("other class");
+}
+
+
+
+for (let i=2; i<=10; i++) {
+    if (i % 2 == 0){
+        output = i
+    }
+    console.log(`2 * ${i} = ${output}`);
 }
